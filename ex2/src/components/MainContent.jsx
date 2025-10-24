be892101e465135b828e2deb7c0c3f3a414513a6
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-24 17:20:01 -0300

    segunda página react

---
+++ ex2/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+        <h2>Main content</h2>
+        <p>This is the main content</p>
+    </div>
+  )
+}
+
+export default MainContent
