4427393cbade9e09d2080258db386ba66ef17dea
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-25 22:48:12 -0300

    exercicio 7

---
+++ ex7/src/components/ProfileCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProfileCard = () => {
+
+    const styles = {
+        backgroundColor: "lightgray",
+        padding: "15px",
+        borderRadius: "8px",
+        color: "black"
+    };
+
+  return (
+    <h1 style={styles}>
+        Profile Card
+    </h1>
+  )
+}
+
+export default ProfileCard
---
+++ ex7/src/components/StyledCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StyledCard = () => {
+  return (
+    <div style={{ backgroundColor: "lightblue", padding: "20px", borderRadius: "10px", color: "darkblue" }}>
+      <h1>Inline Style</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit beatae atque libero iusto. Quas eos totam aspernatur perferendis! Ab, reiciendis tenetur non dicta debitis quam minus maxime quisquam minima deleniti.</p>
+    </div>
+
+
+  )
+}
+
+export default StyledCard
